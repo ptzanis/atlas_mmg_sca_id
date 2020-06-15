7b2ad10054a3e56c79b15a12e8e0ecfa78a174d0
Polyneikis Tzanis <polyneikis.tzanis@cern.ch>
2020-06-15 15:24:21 +0200

    update run

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
-export PYTHONPATH=/afs/cern.ch/user/p/ptzanis/public/PyUAF:$PYTHONPATH
-export LD_LIBRARY_PATH=/afs/cern.ch/user/p/ptzanis/public/PyUAF:$LD_LIBRARY_PATH
+export PYTHONPATH=PyUAF/:$PYTHONPATH
+export LD_LIBRARY_PATH=PyUAF/:$LD_LIBRARY_PATH
 SCRIPT=client.py
 /usr/bin/python $SCRIPT -sector $1
 
